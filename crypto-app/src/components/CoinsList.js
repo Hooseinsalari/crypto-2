3cd65c17cd320d8dd4040c48711e6edf330c113e
Hooseinsalari <hossein.salari002@gmail.com>
2021-11-25 15:57:47 +0330

    little change

--- crypto-app/src/components/CoinsList.js
+++ crypto-app/src/components/CoinsList.js
@@ -9,9 +9,9 @@ const CoinsList = ({coinSymbol, coinName, coinImage, coinPrice, changeCoinPrice,
             <img className={styles.coinImg} src={coinImage} alt="coinImage" />
             <p className={styles.coinName}>{coinName}</p>
             <p className={styles.coinSymbol}>{coinSymbol.toUpperCase()}</p>
-            <p className={styles.marketCap}>{marketCap.toLocaleString()}</p>
+            <p className={styles.marketCap}>{marketCap.toLocaleString()} $</p>
             <p className={changeCoinPrice>0 ?styles.negativePrice :styles.plusPrice  }>{changeCoinPrice.toFixed(2)}</p>
-            <p className={styles.coinPrice}>{coinPrice}</p>
+            <p className={styles.coinPrice}>{coinPrice} $</p>
         </div>
     );
 };
